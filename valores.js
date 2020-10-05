7900325c21005e0d03550719c650c541b431af75
Alejandro <alejandrosantalucia@gmail.com>
2020-10-05 10:24:51 -0300

    primer commit

---
+++ valores.js
@@ -0,0 +1,16 @@
+// number
+40
+
+// string
+"Alejandro Santa Lucia"
+
+// booleano
+true
+false
+
+null
+undefined
+
+// Valores tipo objeto
+[1,2,3]
+{ nombre: "Ale"}
---
+++ variables.js
@@ -0,0 +1,27 @@
+var nombre = "Ale";
+
+//declarar
+var edad; // solo se declara y reversa espacio en memoria, para luego darle un valor
+
+edad    //posisionandonos encima lo marca como undefined
+
+
+//inicializar
+edad = 30; // La variable ya tiene un valor
+
+edad //posisionandonos encima lo marca como number porque ya tiene un valor asignado
+
+
+
+//declarando de array
+
+var elementos= ["computadora","celular"];
+
+//llamando a alguno de sus elementos
+elementos[1]; //el resultado es "celular"
+
+//declarando un objeto
+var persona = {
+    nombre : "Ale",
+    edad : 30
+};
