cfb96d8be4279b367325dd8ad55f4e56dcd14a28
Alejandro <alejandrosantalucia@gmail.com>
2020-10-07 10:03:40 -0300

    ejercicio piedra papel tijera

---
+++ funciones.js
@@ -0,0 +1,15 @@
+// Declarativas
+
+function miFuncion () {
+    return 3;
+}
+
+// Expresion
+
+var miFuncion = function(a,b) {
+    return a + b;
+}
+
+
+var edad = prompt (cuantos años tenes?);
+console.log(edad)
