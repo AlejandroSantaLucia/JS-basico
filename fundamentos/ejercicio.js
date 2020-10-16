681d0c1cd693c6f7c8706120b0f1fca13d088f79
Alejandro <alejandrosantalucia@gmail.com>
2020-10-16 09:13:26 -0300

    clases de prototipos

--- fundamentos/ejercicio.js
+++ fundamentos/ejercicio.js
@@ -12,3 +12,13 @@ function saludar (n,e){
     console.log ( ` Hola, me llamo ${n} y tengo ${e} años`)
 }
 
+saludar (nombre, edad)
+
+-------
+
+function saludar2 (persona) {
+    var {nombre,edad} = persona; // por los corchetes, esto equivale a persona.nombre y persona.edad = persona
+    console.log ( ` Hola, me llamo ${nombre} y tengo ${edad} años`)
+}
+
+saludar (ale) //aca dito que "persona" es "ale", por lo que la funcion dice que {nomnbre,edad} = persona es igual a ale.nombre y ale.edad
--- fundamentos/ejercicioCondicional.js
+++ fundamentos/ejercicioCondicional.js
@@ -6,6 +6,32 @@ var alejandro = {
 function mayoriaDeEdad (alejandro){
 
     if (alejandro.edad >= 18) { console.log (`es mayor de edad`)}
-    else { console-log(`es menor de edad`)}
+    else { console.log(`es menor de edad`)}
 }
 
+matoruaDeEdad (alejandro)
+
+//es mayor de edad
+
+
+
+---------------------------
+// otra alternativa es separar las funciones para que quede mas limpio
+
+const MAYORIA_DE_EDAD = 18 // como esta variable no deberia modificarse, podemos usar la palabra reservada const (constante)
+//sobre las const se acostumbra usar mayusculas para nombrarlas.
+
+function esMayorDeEdad (persona) {
+    return persona.edad >= MAYORIA_DE_EDAD
+}
+
+function pasoMayoria (persona){
+    if (esMayorDeEdad(persona)){
+
+        console.log (`Si, es mayor de edad`)
+    }
+    else {
+        console.log (`No, no es mayor de edad`)
+    }
+
+}
